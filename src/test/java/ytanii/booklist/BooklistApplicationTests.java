9097cca62c8b14e6e4cf796173beed2b7505cfd8
ytanii <154643447+ytanii@users.noreply.github.com>
2026-03-03 21:22:41 -0500

    spring boot setup

---
+++ src/test/java/ytanii/booklist/BooklistApplicationTests.java
@@ -0,0 +1,13 @@
+package ytanii.booklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
